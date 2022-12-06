dd9b6159ad5ff8692a19103422be6ae3b14014bc
AKHIL V S <akhilv.reubro@gmail.com>
2022-12-06 16:39:53 +0530

    06-12-2022 V1.0.1

--- lib/screens/calendar/calendar_controller.dart
+++ lib/screens/calendar/calendar_controller.dart
@@ -1,29 +1,50 @@
-import 'dart:collection';
-
 import 'package:get/get.dart';
+import 'package:intl/intl.dart';
+import 'package:paytym/models/calendar/meeting_response_model.dart';
 import 'package:paytym/network/base_controller.dart';
-import 'package:table_calendar/table_calendar.dart';
 
 import '../../core/constants/enums.dart';
-import '../../models/calendar/calendar_model.dart';
-import 'package:flutter/material.dart';
+import '../../network/base_client.dart';
+import '../../network/end_points.dart';
+import '../login/login_controller.dart';
 
 class CalendarController extends GetxController with BaseController {
   final selectedCalendarTab = CalendarTabs.meeting.obs;
+  final meetingResponseModel = MeetingResponseModel().obs;
 
   final selectedDay = DateTime.now().obs;
 
   List<dynamic> getEventsForDay(DateTime day) {
-    return ['HI', 'This', 'is', 'HI', 'This', 'is'];
+    return ['HI'];
   }
 
-  static List<CalendarModel> kCalendarModelList = [
-    CalendarModel('Mon', '10', Colors.white, Colors.transparent),
-    CalendarModel('Tue', '11', Colors.white, Colors.transparent),
-    CalendarModel('Wed', '12', Colors.white, Colors.transparent),
-    CalendarModel('Thu', '13', Colors.white, Colors.transparent),
-    CalendarModel('Fri', '14', Colors.white, Colors.transparent),
-    CalendarModel('Sat', '15', Colors.white, Colors.transparent),
-    CalendarModel('Sun', '16', Colors.white, Colors.transparent),
-  ];
+  //todo add onError in getshedule as in getmeeting
+
+  getMeeting() async {
+    showLoading();
+    Get.find<BaseClient>().onError = getMeeting;
+    var responseString = await Get.find<BaseClient>()
+        .post(ApiEndPoints.meetings, null,
+            Get.find<LoginController>().getHeader())
+        .catchError(handleError);
+    if (responseString == null) {
+      return;
+    } else {
+      hideLoading();
+      meetingResponseModel.value = meetingResponseModelFromJson(responseString);
+      meetingResponseModel.refresh();
+      Get.find<BaseClient>().onError = null;
+    }
+  }
+
+  getTime(String time) {
+    final DateTime now = DateTime.parse(time);
+    return DateFormat('hh:mm a').format(now);
+  }
+
+  @override
+  void onReady() {
+    super.onReady();
+    getMeeting();
+  }
 }
